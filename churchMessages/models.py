71df0a9d78dc0ad72ee10776a6bba9c9d740d395
Frank Jane Partner <partnermarvel55@gmail.com>
2023-11-19 10:41:44 +0100

    Lines starting with '#' will be ignore

--- churchMessages/models.py
+++ churchMessages/models.py
@@ -1,30 +1,14 @@
 from django.db import models
 from django.utils import timezone
 from django.utils.text import slugify
-from core.models import Category
+from core.models import Category, CommonInfo
 
 # Create your models here.
-class CommonInfo(models.Model):
-    name = models.CharField(max_length=100)
-    class_id = models.PositiveIntegerField()
-    slug = models.SlugField(max_length=20)
-    created_at = models.DateTimeField(default=timezone.now)
-    author = models.CharField(max_length=50, default='')
-
-    def save(self, *args, **kwargs):
-        # If the slug is not provided, create one from the name
-        if not self.slug:
-            self.slug = slugify(self.name)
-        super().save(*args, **kwargs)
-
-    class Meta:
-        abstract = True
-        ordering = ["name"]
-
 class VideoMessage(CommonInfo):
     Category = models.ForeignKey(Category, on_delete=models.CASCADE)
     images = models.ImageField(upload_to="media/videoMessages", height_field=None, width_field=None, max_length=None)
-    link = models.URLField()
+    link = models.URLField(max_length=500, blank=True, null=True)
+    year = models.DateField(null=True)
 
     def __str__(self):
         return f'{self.name}'
@@ -35,7 +19,8 @@ class VideoMessage(CommonInfo):
 class AudioMessage(CommonInfo):
     Category = models.ForeignKey(Category, on_delete=models.CASCADE)
     images = models.ImageField(upload_to="media/AudioMessages", height_field=None, width_field=None, max_length=None)
-    file = models.FileField(upload_to='media/audios', max_length=100)
+    link = models.URLField(max_length=500, blank=True, null=True)
+    year = models.DateField(null=True)
     
     def __str__(self):
         return f'{self.name}'
