9f195432885ebcc72883d28a6722d07a335e3978
Frank Jane Partner <partnermarvel55@gmail.com>
2023-12-03 15:53:01 +0100

    update

--- contact/urls.py
+++ contact/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import contact
+from .views import contact, enquiry, prayers
 
 app_name = 'contact'
 urlpatterns = [
-    path('', contact, name='contact')
+    path('', contact, name='contact'),
+    path('enquiry/', enquiry, name='enquiry'),
+    path('prayers/', prayers, name='prayers')
 ]
--- contact/views.py
+++ contact/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def contact(request):
     return render(request, 'contact.html')
+
+def enquiry(request):
+    return render(request, 'enquiry.html')
+
+def prayers(request):
+    return render(request, 'prayer-request.html')
